6cc44d384259accd88351331ac0de4e206bf24f7
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-17 14:07:19 -0500

    Added DollarStore and DollarItem classes

---
+++ Models/DollarItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace mall
+{
+    public class DollarItem
+    {   
+        public string Name { get; set; }
+
+
+        private double _price = 1.00;
+
+    }
+}
---
+++ Models/DollarStore.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace mall
+{
+    public class DollarStore
+    {
+        private Dictionary<string, DollarItem> _inventory = new Dictionary<string, DollarItem>();
+    }
+}
