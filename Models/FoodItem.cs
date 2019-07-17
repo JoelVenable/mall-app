ed9a771c2217305ca6940017b53b880620fc0478
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-17 14:14:05 -0500

    Added food store and food item classes'

---
+++ Models/FoodItem.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MallApp
+{
+    public class FoodItem
+    {
+        public string Name { get; set; }
+
+        public int Calories { get; set; } 
+
+        public double Price { get; set;  }
+
+    }
+
+}
---
+++ Models/FoodStore.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace  MallApp
+{
+    public class FoodStore
+    {
+        private Dictionary<string, FoodItem> _inventory = new Dictionary<string, FoodItem>();
+    }
+}
